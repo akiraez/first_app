037bc4d5c23e5f682f83e8eb0e6f532c0a9f38ef
vagrant <vagrant@localhost.localdomain>
2020-07-01 10:17:34 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   root 'homes#top'
 
-  resources :users
-  resources :books
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :relationships, only: [:create, :destroy]
+  resources :books, only: [:index, :show, :edit, :update, :create, :destroy] do
+  resources :favorites, only: [:create, :destroy]
+  resource :book_comments, only: [:create, :destroy]
+end
 end
